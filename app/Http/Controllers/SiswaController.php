8821b44429e6ad765258f9565b8f28e4ecedc865
Arfian Dimas Andi Permana <arfiandimas1929@gmail.com>
2020-02-01 02:14:32 +0700

    mengedit file SiswaController.php dan route api.php

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -12,9 +12,10 @@ class SiswaController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($id)
     {
-        return Siswa::all();
+        $siswa = Siswa::find($id);
+        return $siswa;
     }
 
     /**
